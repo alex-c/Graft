96d5e4bb608c319f93e56cc75db3516ab07366a5
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-04-09 20:24:13 +0200

    MST setup

---
+++ src/Graft.Algorithms/MinimumSpanningTree/Prim.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Graft.Algorithms.MinimumSpanningTree
+{
+    public static class Prim
+    {
+        public static IGraph<T> FindMinimumSpanningTree<T>(IGraph<T> graph)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
