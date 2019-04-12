c3043ec22ee33c3347ba1831fb73f53434e9a3d6
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-04-12 10:53:01 +0200

    Implemented naive Kruskal
    Currently doesn't work because of the way edges are handled in the default graph data structure...

--- src/Graft.Algorithms/MinimumSpanningTree/Kruskal.cs
+++ src/Graft.Algorithms/MinimumSpanningTree/Kruskal.cs
@@ -11,29 +11,37 @@ namespace Graft.Algorithms.MinimumSpanningTree
     {
         public static IWeightedGraph<TV, TW> FindMinimumSpanningTree<TV, TW>(IWeightedGraph<TV, TW> graph) where TV : IEquatable<TV>
         {
-            throw new NotImplementedException();
-            //GraphBuilder<TV, TW> builder = new GraphBuilder<TV, TW>();
+            // Builder used to incrementally build the target minimum spanning tree
+            GraphBuilder<TV, TW> builder = new GraphBuilder<TV, TW>();
 
-            //// Get all verteces and edges of graph
-            //IEnumerable<IVertex<TV>> verteces = graph.GetVerteces();
-            //IEnumerable<IWeightedEdge<TV, TW>> edges = graph.GetEdges();
+            // Get all verteces and edges of graph
+            IEnumerable<IVertex<TV>> verteces = graph.GetAllVerteces();
+            IEnumerable<IWeightedEdge<TV, TW>> edges = graph.GetAllEdges();
 
-            //// Forest of verteces
-            //builder.AddVerteces(verteces.Select(v => v.Value));
+            // Add original forest of verteces
+            builder.AddVerteces(verteces.Select(v => v.Value));
 
-            //// Sorted collection of edges
-            //HashSet<IWeightedEdge<TV, TW>> sortedEdges = new HashSet<IWeightedEdge<TV, TW>>(edges.OrderBy(e => e.Weight));
+            // Sorted collection of edges
+            Queue<IWeightedEdge<TV, TW>> sortedEdges = new Queue<IWeightedEdge<TV, TW>>(edges.OrderBy(e => e.Weight));
 
-            //// Apply Krsukal
-            //IWeightedGraph<TV, TW> tempGraph = null;
-            //while (sortedEdges.Any())
-            //{
-            //    tempGraph = builder.Build();
-            //    IWeightedEdge<TV, TW> nextEdge = sortedEdges.First();
-            //    sortedEdges.Remove(nextEdge);
+            // Apply Krsukal
+            IWeightedGraph<TV, TW> tempGraph = null;
+            while (sortedEdges.Any())
+            {
+                // Get most cost-efficient remaining edge
+                IWeightedEdge<TV, TW> nextEdge = sortedEdges.Dequeue();
 
-            //    BreadthFirstSearch.Search(tempGraph, nextEdge.OriginVertex, v => { if (v.Value == nextEdge.TargetVertex.Value) { } });
-            //}
+                // Check whether the verteces connected by this edge are already connected in the target graph
+                tempGraph = builder.Build();
+                if (BreadthFirstSearch.Search(tempGraph, nextEdge.OriginVertex, v => v.Value.Equals(nextEdge.TargetVertex.Value)) == null)
+                {
+                    // Verteces are not connected yet, add edge to target graph
+                    builder.AddEdge(nextEdge.OriginVertex.Value, nextEdge.TargetVertex.Value, nextEdge.Weight);
+                }
+            }
+
+            // No verteces left to check, build and return target graph
+            return builder.Build();
         }
     }
 }
---
+++ test/Graft.Algorithms.Test/MinimumSpanningTreeTests.cs
@@ -0,0 +1,39 @@
+﻿using Graft.Algorithms.MinimumSpanningTree;
+using Graft.Default;
+using Graft.Default.File;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Graft.Algorithms.Tests
+{
+    [TestClass]
+    public class MinimumSpanningTreeTests
+    {
+        private GraphFactory<int, double> Factory { get; }
+
+        private DefaultGraphTextLineParser Parser { get; }
+
+        public MinimumSpanningTreeTests()
+        {
+            Factory = new GraphFactory<int, double>();
+            Parser = new DefaultGraphTextLineParser();
+        }
+
+        [TestMethod]
+        public void TestTinyGraph()
+        {
+            IWeightedGraph<int, double> graph = ReadGraphFromFile("./graphs/weighted/G_1_2.txt");
+
+            IWeightedGraph<int, double> msp = Kruskal.FindMinimumSpanningTree(graph);
+
+            double mspWeight = msp.GetAllEdges().Sum(e => e.Weight);
+
+            Assert.AreEqual(286.711, mspWeight);
+        }
+
+        private IWeightedGraph<int, double> ReadGraphFromFile(string filePath)
+        {
+            return Factory.CreateGraphFromFile(filePath, Parser, false);
+        }
+    }
+}
