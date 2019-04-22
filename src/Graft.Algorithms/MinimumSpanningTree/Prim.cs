ae31e5bdd15708675def13647494064390721a7f
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-04-22 22:49:49 +0200

    Corrected Prim algorithm signature

--- src/Graft.Algorithms/MinimumSpanningTree/Prim.cs
+++ src/Graft.Algorithms/MinimumSpanningTree/Prim.cs
@@ -2,9 +2,19 @@
 
 namespace Graft.Algorithms.MinimumSpanningTree
 {
+    /// <summary>
+    /// An implementation of Prim's algorithm.
+    /// </summary>
     public static class Prim
     {
-        public static IGraph<T> FindMinimumSpanningTree<T>(IGraph<T> graph) where T : IEquatable<T>
+        /// <summary>
+        /// Builds the minimum spanning tree of a given graph.
+        /// </summary>
+        /// <typeparam name="TV">Type of the graph vertex values.</typeparam>
+        /// <typeparam name="TW">Type of the graph edge weights.</typeparam>
+        /// <param name="graph">The graph got whcih to build the minimum spanning tree.</param>
+        /// <returns>Returns the minimum spanning tree of the given graph.</returns>
+        public static IWeightedGraph<TV, TW> FindMinimumSpanningTree<TV, TW>(IWeightedGraph<TV, TW> graph) where TV : IEquatable<TV>
         {
             throw new NotImplementedException();
         }
