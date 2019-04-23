ffcf24837e67f5cc42df95777cdf4072c2b65e52
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-04-23 22:57:44 +0200

    Refactored MSP tests

--- test/Graft.Algorithms.Test/MinimumSpanningTreeTests.cs
+++ test/Graft.Algorithms.Test/MinimumSpanningTreeTests.cs
@@ -2,6 +2,7 @@
 using Graft.Default;
 using Graft.Default.File;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Linq;
 
 namespace Graft.Algorithms.Tests
@@ -23,77 +24,79 @@ namespace Graft.Algorithms.Tests
         public void TestTinyGraph()
         {
             IWeightedGraph<int, double> graph = ReadGraphFromFile("./graphs/weighted/G_1_2.txt");
-
-            IWeightedGraph<int, double> msp = Kruskal.FindMinimumSpanningTree(graph);
-
-            double mspWeight = msp.GetAllEdges().Sum(e => e.Weight);
-
-            AssertDoublesNearlyEqual(286.711, mspWeight, 0.001);
+            TestMstAlgorithms(graph, 286.711, 0.001);
         }
 
         [TestMethod]
         public void TestSmallGraph()
         {
             IWeightedGraph<int, double> graph = ReadGraphFromFile("./graphs/weighted/G_1_20.txt");
-
-            IWeightedGraph<int, double> msp = Kruskal.FindMinimumSpanningTree(graph);
-
-            double mspWeight = msp.GetAllEdges().Sum(e => e.Weight);
-
-            AssertDoublesNearlyEqual(29.5493, mspWeight, 0.0001);
+            TestMstAlgorithms(graph, 29.5493, 0.0001);
         }
 
         [TestMethod]
         public void TestMediumGraph()
         {
             IWeightedGraph<int, double> graph = ReadGraphFromFile("./graphs/weighted/G_1_200.txt");
-
-            IWeightedGraph<int, double> msp = Kruskal.FindMinimumSpanningTree(graph);
-
-            double mspWeight = msp.GetAllEdges().Sum(e => e.Weight);
-
-            AssertDoublesNearlyEqual(3.0228, mspWeight, 0.0001);
+            TestMstAlgorithms(graph, 3.0228, 0.0001);
         }
 
         [TestMethod]
         public void TestBigGraph()
         {
             IWeightedGraph<int, double> graph = ReadGraphFromFile("./graphs/weighted/G_10_20.txt");
-
-            IWeightedGraph<int, double> msp = Kruskal.FindMinimumSpanningTree(graph);
-
-            double mspWeight = msp.GetAllEdges().Sum(e => e.Weight);
-
-            AssertDoublesNearlyEqual(2775.44, mspWeight, 0.01);
+            TestMstAlgorithms(graph, 2775.44, 0.01);
         }
 
         [TestMethod]
         public void TestVeryBigGraph()
         {
             IWeightedGraph<int, double> graph = ReadGraphFromFile("./graphs/weighted/G_10_200.txt");
-
-            IWeightedGraph<int, double> msp = Kruskal.FindMinimumSpanningTree(graph);
-
-            double mspWeight = msp.GetAllEdges().Sum(e => e.Weight);
-
-            AssertDoublesNearlyEqual(301.552, mspWeight, 0.001);
+            TestMstAlgorithms(graph, 301.552, 0.001);
         }
 
         [TestMethod]
         public void TestHugeGraph()
         {
             IWeightedGraph<int, double> graph = ReadGraphFromFile("./graphs/weighted/G_100_200.txt");
-
-            IWeightedGraph<int, double> msp = Kruskal.FindMinimumSpanningTree(graph);
-
-            double mspWeight = msp.GetAllEdges().Sum(e => e.Weight);
-
-            AssertDoublesNearlyEqual(27450.6, mspWeight, 0.1);
+            TestMstAlgorithms(graph, 27450.6, 0.1);
         }
 
         private IWeightedGraph<int, double> ReadGraphFromFile(string filePath)
         {
             return Factory.CreateGraphFromFile(filePath, Parser, false);
+        }
+
+        private void TestMstAlgorithms(IWeightedGraph<int, double> graph, double expectedMstWeight, double precision)
+        {
+            TestMstAlgorithm(graph, expectedMstWeight, precision, ALGORITHMS.KRUSKAL);
+            TestMstAlgorithm(graph, expectedMstWeight, precision, ALGORITHMS.PRIM);
+        }
+
+        private void TestMstAlgorithm(IWeightedGraph<int, double> graph, double expectedMstWeight, double precision, ALGORITHMS algorithm)
+        {
+            // Build minimum spanning tree with algorithm to test
+            IWeightedGraph<int, double> msp;
+            switch (algorithm)
+            {
+                case ALGORITHMS.KRUSKAL:
+                    msp = Kruskal.FindMinimumSpanningTree(graph);
+                    break;
+                case ALGORITHMS.PRIM:
+                    msp = Prim.FindMinimumSpanningTree(graph, 0, double.MaxValue);
+                    break;
+                default:
+                    throw new NotImplementedException($"No minimum spanning tree test implemented for algorithm {algorithm.ToString()}.");
+            }
+
+            // Check that all verteces are included
+            Assert.AreEqual(graph.VertexCount, msp.VertexCount);
+
+            // Compute total weight
+            double mspWeight = msp.GetAllEdges().Sum(e => e.Weight);
+
+            // Check that the total weight is as expected
+            AssertDoublesNearlyEqual(expectedMstWeight, mspWeight, precision);
         }
 
         private void AssertDoublesNearlyEqual(double expected, double actual, double precision)
@@ -103,5 +106,11 @@ namespace Graft.Algorithms.Tests
                 throw new AssertFailedException($"Doubles are not equal with a precision of {precision}. Expected {expected}, got {actual}.");
             }
         }
+    }
+
+    internal enum ALGORITHMS
+    {
+        KRUSKAL,
+        PRIM
     }
 }
