2fad7823ac9c06f011aeccf086ec5e050df0c303
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-04-23 23:38:22 +0200

    Refactored MSP lab test runner

--- labs/Graft.Labs.MinimumSpanningTree/TestRunner.cs
+++ labs/Graft.Labs.MinimumSpanningTree/TestRunner.cs
@@ -34,38 +34,66 @@ namespace Graft.Labs.MinimumSpanningTree
 
         public void RunTests()
         {
-            Console.WriteLine("Loading graphs from file...");
-            foreach (string file in Files)
-            {
-                if (!Graphs.ContainsKey(file))
-                {
-                    Console.WriteLine($" + loading '{file}'...");
-                    Graphs.Add(file, ReadGraphFromFile(file));
-                }
-            }
-            Console.WriteLine("Done loading graphs from file.\n");
-
+            ReadGraphsFromFile();
             Console.WriteLine("Run minimum spanning tree tests...");
             foreach (KeyValuePair<string, IWeightedGraph<int, double>> graph in Graphs)
             {
-                RunTest(graph.Key, graph.Value);
+                RunTest(graph.Key, graph.Value, ALGORITHM.KRUSKAL);
+                RunTest(graph.Key, graph.Value, ALGORITHM.PRIM);
             }
             Console.WriteLine("Done running minimum spanning tree tests.");
         }
 
-        private void RunTest(string file, IWeightedGraph<int, double> graph)
+        private void ReadGraphsFromFile()
+        {
+            Console.WriteLine("Reading graphs from file...");
+            foreach (string file in Files)
+            {
+                try
+                {
+                    if (!Graphs.ContainsKey(file))
+                    {
+                        Console.WriteLine($" - loading '{file}'...");
+                        Graphs.Add(file, ReadGraphFromFile(file));
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($" ! Error while parsing '{file}': {exception.Message}");
+                }
+            }
+            Console.WriteLine("Done reading graphs from file.\n");
+        }
+
+        private void RunTest(string file, IWeightedGraph<int, double> graph, ALGORITHM algorithm)
         {
             Stopwatch sw = new Stopwatch();
-            sw.Start();
-            Algorithms.MinimumSpanningTree.Kruskal.FindMinimumSpanningTree(graph);
-            sw.Stop();
+            switch (algorithm)
+            {
+                case ALGORITHM.KRUSKAL:
+                    sw.Start();
+                    Algorithms.MinimumSpanningTree.Kruskal.FindMinimumSpanningTree(graph);
+                    sw.Stop();
+                    break;
+                case ALGORITHM.PRIM:
+                    sw.Start();
+                    Algorithms.MinimumSpanningTree.Prim.FindMinimumSpanningTree(graph, 0, double.MaxValue);
+                    sw.Stop();
+                    break;
+            }
 
-            Console.WriteLine($" + Computed minimum spanning tree of '{file}' using Kruskal's algorithm in {sw.Elapsed}.");
+            Console.WriteLine($" + Computed minimum spanning tree of '{file}' using {algorithm.ToString()}'s algorithm in {sw.Elapsed}.");
         }
 
         private IWeightedGraph<int, double> ReadGraphFromFile(string fileName)
         {
             return Factory.CreateGraphFromFile($"./graphs/{fileName}", Parser, false);
         }
+    }
+
+    internal enum ALGORITHM
+    {
+        KRUSKAL,
+        PRIM
     }
 }
