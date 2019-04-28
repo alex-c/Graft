42c2aeef306e83549c6698eaa81872d8e149d1ae
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-04-28 22:08:59 +0200

    Added djikstra signature

---
+++ src/Graft.Algorithms/ShortestPath/Dijkstra.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Graft.Algorithms.ShortestPath
+{
+    public static class Dijkstra
+    {
+        public static IWeightedGraph<TV, TW> FindShortestPathTree<TV, TW>(IWeightedGraph<TV, TW> graph) where TV : IEquatable<TV>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
