2d66c55ce6ddb99dd5fe040764a19738904891b8
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-04-29 12:20:13 +0200

    Implemented a priority queue based on fibonacci heap

---
+++ src/Graft.DataStructures/PriorityQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Graft.DataStructures
+{
+    public class PriorityQueue<TE, TP> : IPriorityQueue<TE, TP> where TP : IComparable
+    {
+        private FibonacciHeap<TE, TP> Heap { get; }
+
+        public bool Empty => Heap.Empty;
+
+        public int Count => Heap.Count;
+
+        public PriorityQueue()
+        {
+            Heap = new FibonacciHeap<TE, TP>();
+        }
+
+        public bool Contains(TE element)
+        {
+            return Heap.Contains(element);
+        }
+
+        public TP GetPriorityOf(TE element)
+        {
+            return Heap.GetPriorityOf(element);
+        }
+
+        public void Enqueue(TE element, TP priority)
+        {
+            Heap.Insert(element, priority);
+        }
+
+        public TE Dequeue()
+        {
+            return Heap.ExtractMinimum();
+        }
+
+        public void UpdatePriority(TE element, TP priority)
+        {
+            Heap.DecreaseKey(element, priority);
+        }
+    }
+}
