0705be64af83ac5850e4b5cadb559dee617e21f6
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-05-03 11:24:33 +0200

    Added "graph not complete" exception

---
+++ src/Graft.Algorithms/TravelingSalesmanProblem/GraphNotCompleteException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Graft.Algorithms.TravelingSalesmanProblem
+{
+    public class GraphNotCompleteException : Exception
+    {
+        public GraphNotCompleteException() { }
+
+        public GraphNotCompleteException(string message) : base(message) { }
+
+        public GraphNotCompleteException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected GraphNotCompleteException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
