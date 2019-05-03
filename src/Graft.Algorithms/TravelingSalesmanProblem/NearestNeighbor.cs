73e1f1d159bcaa56e6103f1415fb47a3270d1ea3
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-05-03 11:24:09 +0200

    Fixed nearest neighbor algorithm

--- src/Graft.Algorithms/TravelingSalesmanProblem/NearestNeighbor.cs
+++ src/Graft.Algorithms/TravelingSalesmanProblem/NearestNeighbor.cs
@@ -13,17 +13,18 @@ namespace Graft.Algorithms.TravelingSalesmanProblem
             return FindTour(graph, graph.GetFirstVertex());
         }
 
+        // TODO: should the result graph connect to the source vertex again (= be a cycle)
         public static IWeightedGraph<TV, TW> FindTour<TV, TW>(IWeightedGraph<TV, TW> graph, IVertex<TV> source) where TV : IEquatable<TV>
         {
             GraphBuilder<TV, TW> builder = new GraphBuilder<TV, TW>();
 
-            // Track which verteces have already been added to the tour, and how many edges have already been used
-            HashSet<TV> visitedVerteces = new HashSet<TV>();
-            int edgesUsed = 0;
-
             // Starting point
             IVertex<TV> currentVertex = source;
             builder.AddVertex(source.Value);
+
+            // Track which verteces have already been added to the tour, and how many edges have already been used
+            HashSet<TV> visitedVerteces = new HashSet<TV>() { source.Value };
+            int edgesUsed = 0;
 
             // Find and use minimum non-used edge leading from each vertex
             while (edgesUsed < graph.VertexCount - 1)
@@ -31,11 +32,23 @@ namespace Graft.Algorithms.TravelingSalesmanProblem
                 IWeightedEdge<TV, TW> minEdge = graph.GetEdgesOfVertex(currentVertex)
                     .Where(e => !visitedVerteces.Contains(e.ConnectedVertex(currentVertex).Value))
                     .OrderBy(e => e.Weight)
-                    .First();
+                    .FirstOrDefault();
+                if (minEdge == null)
+                {
+                    throw new GraphNotCompleteException("The NearestNeighbor algorithm expects the input graph to be complete!");
+                }
+
+                // Get target vertex and update current vertex
                 IVertex<TV> target = minEdge.ConnectedVertex(currentVertex);
-                builder.AddVertex(target.Value);
-                builder.AddEdge(source.Value, target.Value);
+                currentVertex = target;
+
+                // Update tracking
                 visitedVerteces.Add(target.Value);
+                edgesUsed++;
+
+                // Update result graph
+                builder.AddVertex(target.Value);
+                builder.AddEdge(source.Value, target.Value, minEdge.Weight);
             }
 
             // Done: used n-1 edges for n verteces
