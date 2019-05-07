b11138a6e3e42dd51cf69a14165f105b6dd8fdca
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-05-07 17:56:06 +0200

    Added brute force TSP signature

---
+++ src/Graft.Algorithms/TravelingSalesmanProblem/BruteForce.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Graft.Algorithms.TravelingSalesmanProblem
+{
+    public static class BruteForce
+    {
+        public static IWeightedGraph<TV, TW> FindOptimalTour<TV, TW>(IWeightedGraph<TV, TW> graph) where TV : IEquatable<TV>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
