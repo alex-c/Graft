482fd1ecab5408fc60ce063631bcf17ac4b235b8
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-05-10 11:56:58 +0200

    Added Bellman-Ford-Moore algorithm signature

---
+++ src/Graft.Algorithms/ShortestPath/BellmanFordMoore.cs
@@ -0,0 +1,15 @@
+﻿using Graft.Primitives;
+using System;
+
+namespace Graft.Algorithms.ShortestPath
+{
+    public static class BellmanFordMoore
+    {
+        public static IWeightedGraph<TV, TW> FindShortestPath<TV, TW>(IWeightedGraph<TV, TW> graph, IVertex<TV> source, IVertex<TV> target)
+            where TV : IEquatable<TV>
+            where TW : IComparable
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
