ac7de9e51ab807ab1338ed227c02925f928deee1
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-05-14 21:18:22 +0200

    Extended shortest path tests

--- test/Graft.Algorithms.Test/ShortestPathTests.cs
+++ test/Graft.Algorithms.Test/ShortestPathTests.cs
@@ -25,7 +25,7 @@ namespace Graft.Algorithms.Tests
         [TestMethod]
         public void TestShortestPath1()
         {
-            Graph<int, double> graph = Factory.CreateGraphFromFile("./graphs/paths/Wege1.txt", Parser);
+            Graph<int, double> graph = Factory.CreateGraphFromFile("./graphs/paths/Wege1.txt", Parser, true);
             IVertex<int> source = graph.GetFirstMatchingVertex(v => v.Value == 2);
             IVertex<int> target = graph.GetFirstMatchingVertex(v => v.Value == 0);
             TestShortestPathsAlgorithms(graph, source, target, 6);
@@ -34,9 +34,12 @@ namespace Graft.Algorithms.Tests
         [TestMethod]
         public void TestShortestPath2()
         {
-            Graph<int, double> graph = Factory.CreateGraphFromFile("./graphs/paths/Wege2.txt", Parser);
+            Graph<int, double> graph = Factory.CreateGraphFromFile("./graphs/paths/Wege2.txt", Parser, true);
             IVertex<int> source = graph.GetFirstMatchingVertex(v => v.Value == 2);
             IVertex<int> target = graph.GetFirstMatchingVertex(v => v.Value == 0);
+            TestShortestPathsAlgorithm(graph, source, target, 2, ShortestPathAlgorithm.BellmanFordMoore);
+
+            // Dijkstra fails for graphs with megative edges
             try
             {
                 TestShortestPathsAlgorithm(graph, source, target, 2, ShortestPathAlgorithm.Dijkstra);
@@ -47,34 +50,57 @@ namespace Graft.Algorithms.Tests
         [TestMethod]
         public void TestShortestPath3()
         {
-            Graph<int, double> graph = Factory.CreateGraphFromFile("./graphs/paths/Wege3.txt", Parser);
+            Graph<int, double> graph = Factory.CreateGraphFromFile("./graphs/paths/Wege3.txt", Parser, true);
             IVertex<int> source = graph.GetFirstMatchingVertex(v => v.Value == 2);
             IVertex<int> target = graph.GetFirstMatchingVertex(v => v.Value == 0);
-            TestShortestPathsAlgorithms(graph, source, target, 6); // TODO: graph contains a negative cycle...
+
+            // Dijkstra fails for graphs with megative edges
+            try
+            {
+                TestShortestPathsAlgorithm(graph, source, target, 9000, ShortestPathAlgorithm.Dijkstra);
+            }
+            catch (NegativeEdgeWeightException) { /* Expected exception */ }
+
+            // Bellman-Ford-Moore fails for graphs with megative cycles
+            try
+            {
+                TestShortestPathsAlgorithm(graph, source, target, 9000, ShortestPathAlgorithm.BellmanFordMoore);
+            }
+            catch (NegativeCycleException) { /* Expected exception */ }
         }
 
         [TestMethod]
         public void TestShortestPathForBigGraph()
         {
-            // Undirected graph
-            Graph<int, double> graphUnDirected = Factory.CreateGraphFromFile("./graphs/weighted/G_1_2.txt", Parser);
-            IVertex<int> source = graphUnDirected.GetFirstMatchingVertex(v => v.Value == 0);
-            IVertex<int> target = graphUnDirected.GetFirstMatchingVertex(v => v.Value == 1);
-            TestShortestPathsAlgorithms(graphUnDirected, source, target, 2.36796);
-
             // Directed graph
             Graph<int, double> graphDirected = Factory.CreateGraphFromFile("./graphs/weighted/G_1_2.txt", Parser, true);
-            source = graphDirected.GetFirstMatchingVertex(v => v.Value == 0);
-            target = graphDirected.GetFirstMatchingVertex(v => v.Value == 1);
-            TestShortestPathsAlgorithms(graphDirected, source, target, 5.54417);
+            IVertex<int> source = graphDirected.GetFirstMatchingVertex(v => v.Value == 0);
+            IVertex<int> target = graphDirected.GetFirstMatchingVertex(v => v.Value == 1);
+            TestShortestPathsAlgorithms(graphDirected, source, target, 5.54417, 0.00001);
+
+            // Undirected graph
+            Graph<int, double> graphUnDirected = Factory.CreateGraphFromFile("./graphs/weighted/G_1_2.txt", Parser);
+            source = graphUnDirected.GetFirstMatchingVertex(v => v.Value == 0);
+            target = graphUnDirected.GetFirstMatchingVertex(v => v.Value == 1);
+            TestShortestPathsAlgorithms(graphUnDirected, source, target, 2.36796, 0.00001);
         }
 
-        private void TestShortestPathsAlgorithms(IWeightedGraph<int, double> graph, IVertex<int> source, IVertex<int> target, double pathCosts)
+        private void TestShortestPathsAlgorithms(IWeightedGraph<int, double> graph,
+            IVertex<int> source,
+            IVertex<int> target,
+            double pathCosts,
+            double precision = 0.1)
         {
-            TestShortestPathsAlgorithm(graph, source, target, pathCosts, ShortestPathAlgorithm.Dijkstra);
+            TestShortestPathsAlgorithm(graph, source, target, pathCosts, ShortestPathAlgorithm.BellmanFordMoore, precision);
+            TestShortestPathsAlgorithm(graph, source, target, pathCosts, ShortestPathAlgorithm.Dijkstra, precision);
         }
 
-        private void TestShortestPathsAlgorithm(IWeightedGraph<int, double> graph, IVertex<int> source, IVertex<int> target, double pathCosts, ShortestPathAlgorithm algorithm)
+        private void TestShortestPathsAlgorithm(IWeightedGraph<int, double> graph,
+            IVertex<int> source,
+            IVertex<int> target,
+            double pathCosts,
+            ShortestPathAlgorithm algorithm,
+            double precision = 0.1)
         {
             IWeightedGraph<int, double> shortestPath = null;
             switch (algorithm)
@@ -82,15 +108,27 @@ namespace Graft.Algorithms.Tests
                 case ShortestPathAlgorithm.Dijkstra:
                     shortestPath = Dijkstra.FindShortestPath(graph, source, target, 0.0);
                     break;
+                case ShortestPathAlgorithm.BellmanFordMoore:
+                    shortestPath = BellmanFordMoore.FindShortestPath(graph, source, target, 0.0, double.MaxValue, (x, y) => x + y);
+                    break;
                 default:
                     throw new NotSupportedException($"Testing shortest path for the {algorithm} algorithm is currently not supported.");
             }
-            Assert.AreEqual(pathCosts, shortestPath.GetAllEdges().Sum(e => e.Weight));
+            AssertDoublesNearlyEqual(pathCosts, shortestPath.GetAllEdges().Sum(e => e.Weight), precision);
+        }
+
+        private void AssertDoublesNearlyEqual(double expected, double actual, double precision)
+        {
+            if (actual < expected - precision || actual > expected + precision)
+            {
+                throw new AssertFailedException($"Doubles are not equal with a precision of {precision}. Expected {expected}, got {actual}.");
+            }
         }
     }
 
     internal enum ShortestPathAlgorithm
     {
-        Dijkstra
+        Dijkstra,
+        BellmanFordMoore
     }
 }
