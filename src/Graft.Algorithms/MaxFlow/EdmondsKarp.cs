5a705d2e0b7ac262ad593309ef5bb62b81c1cd4a
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-06-03 20:34:47 +0200

    Added preliminary Edmonds-Karp max flow signature

---
+++ src/Graft.Algorithms/MaxFlow/EdmondsKarp.cs
@@ -0,0 +1,15 @@
+﻿using Graft.Primitives;
+using System;
+
+namespace Graft.Algorithms.MaxFlow
+{
+    public static class EdmondsKarp
+    {
+        public static IWeightedGraph<TV, TW> FindMaxFlow<TV, TW>(IWeightedGraph<TV, TW> graph,
+            IVertex<TV> source,
+            IVertex<TV> target) where TV : IEquatable<TV>
+        {
+            throw new NotImplementedException("TODO: implement this!");
+        }
+    }
+}
