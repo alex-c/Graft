12838d14820cf89e6bfd9d12d8886a38bec78f91
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-06-04 09:19:07 +0200

    Extended primitive attribute interface

--- src/Graft/Default/Edge.cs
+++ src/Graft/Default/Edge.cs
@@ -73,12 +73,38 @@ namespace Graft.Default
             }
         }
 
+        public TAttr GetAttribute<TAttr>(string attribute)
+        {
+            if (Attributes.TryGetValue(attribute, out object value))
+            {
+                return (TAttr)value;
+            }
+            else
+            {
+                throw new KeyNotFoundException($"There is no attribute '{attribute}' set on this vertex.");
+            }
+        }
+
         public void SetAttribute(string attribute, object value)
         {
             Attributes[attribute] = value;
         }
 
         public bool TryGetAttribute(string attribute, out object value) => Attributes.TryGetValue(attribute, out value);
+
+        public bool TryGetAttribute<TAttr>(string attribute, out TAttr value)
+        {
+            if (Attributes.TryGetValue(attribute, out object ovalue))
+            {
+                value = (TAttr)ovalue;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
 
         #endregion
     }
--- src/Graft/Primitives/IPrimitive.cs
+++ src/Graft/Primitives/IPrimitive.cs
@@ -6,8 +6,12 @@
 
         object GetAttribute(string attribute);
 
+        T GetAttribute<T>(string attribute);
+
         void SetAttribute(string attribute, object value);
 
         bool TryGetAttribute(string attribute, out object value);
+
+        bool TryGetAttribute<T>(string attribute, out T value);
     }
 }
