b96cf91980d3a195947dbe7ea14bf55117a7c4bd
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-06-07 09:59:51 +0200

    Added cycle canceling algorithm signature

---
+++ src/Graft.Algorithms/MinFlow/CycleCanceling.cs
@@ -0,0 +1,17 @@
+﻿using Graft.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graft.Algorithms.MinFlow
+{
+    public static class CycleCanceling
+    {
+        public static IWeightedGraph<TV, TW> FindCostMinimalFlow<TV, TW>(IWeightedGraph<TV, TW> graph,
+            IVertex<TV> source,
+            IVertex<TV> target) where TV : IEquatable<TV> where TW : IComparable
+        {
+            throw new NotImplementedException("TODO: implement this!");
+        }
+    }
+}
