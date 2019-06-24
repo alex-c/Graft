4afad84c227f3be98c31ad92e35b8cfc13446085
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-06-24 22:32:02 +0200

    Added successive shortest path algorithm

---
+++ src/Graft.Algorithms/MinCostFlow/SuccessiveShortestPath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graft.Algorithms.MinCostFlow
+{
+    public static class SuccessiveShortestPath
+    {
+        public static IWeightedGraph<TV, TW> FindCostMinimalFlow<TV, TW>(IWeightedGraph<TV, TW> graph,
+            TV superSourceValue,
+            TV superTargetValue,
+            Func<TW, TW, TW> combineValues,
+            Func<TW, TW, TW> substractValues,
+            Func<TW, TW> negateValue,
+            TW zeroValue,
+            TW maxValue)
+            where TV : IEquatable<TV> where TW : IComparable
+        {
+            throw new NotImplementedException("TODO");
+        }
+    }
+}
