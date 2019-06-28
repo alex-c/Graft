9a028957f56e0c3c8d028ba510a2cf2b4fff24d6
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-06-28 10:25:18 +0200

    Added max matching algorithm signature

---
+++ src/Graft.Algorithms/MaxMatchings/BipartiteMatching.cs
@@ -0,0 +1,14 @@
+﻿using Graft.Primitives;
+using System;
+using System.Collections.Generic;
+
+namespace Graft.Algorithms.MaxMatchings
+{
+    public static class BipartiteMatching
+    {
+        public static IEnumerable<IEdge<TV>> FindMaxMatching<TV>(IGraph<TV> graph) where TV : IEquatable<TV>
+        {
+            throw new NotImplementedException("TODO: implement this!");
+        }
+    }
+}
